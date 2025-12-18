9d3f608eb73cc86e1e3c30847cdc08bef868ba8f
nguyenhoangnhi279 <127406975+nguyenhoangnhi279@users.noreply.github.com>
2025-12-18 23:25:47 +0700

    Update main.cpp

--- server/main.cpp
+++ server/main.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main() {
     ServerNetwork server;
     
-    // Nếu máy mày bị trùng port, đổi 8080 thành 9000 (nhớ đổi cả trong index.html)
     if (!server.Init(8080)) {
         cout << ">> Loi: Khong the mo Port 8080. (Co the do Skype/Discord dang chiem?)" << endl;
         cin.get();
